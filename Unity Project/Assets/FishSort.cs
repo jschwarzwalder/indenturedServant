15ad21fd9ee7269b82a15f9fa528d32c0bfc9447
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-03-26 21:08:45 -0700

    Buckets now count the number of fish in them.

---
+++ Unity Project/Assets/FishSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.VR.WSA;
+
+public class FishSort : MonoBehaviour
+{
+
+    public int sorted_fish;
+    public FishData sorted_object; 
+    
+
+	// Use this for initialization
+	void Start ()
+	{
+	    sorted_fish = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        FishData data = other.GetComponent<FishData>();
+        if (data != null && data.type.Equals( sorted_object.type))
+        {
+            //count, set timer to destroy
+            sorted_fish++;
+        }
+    }
+
+    
+}
