faeb1fadfe7e7bd255292f0794f69889df7faf33
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-03-26 18:22:02 -0700

    Build Countdown Timer

---
+++ Unity Project/Assets/Scripts/Countdown.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+    public float duration = 60;
+    public float timeRemaining;
+    public bool isCountingDown = false;
+    public void Start()
+    {
+        if (!isCountingDown)
+        {
+            isCountingDown = true;
+            timeRemaining = duration;
+            
+        }
+    }
+
+    private void Update()
+    {
+        if (isCountingDown) {
+            timeRemaining -= Time.deltaTime;
+
+           if (timeRemaining<= 0) {
+               timeRemaining = 0;
+               isCountingDown = false;
+            }
+
+            GetComponent<Text>().text = "Time Remaining: \n" + timeRemaining;
+        }
+    }
+}
