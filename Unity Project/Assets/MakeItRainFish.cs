408f0e0d0ea1937fc90af1416bdab97673e45a6d
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-05-29 00:31:29 -0700

    fish spawn every 3 seconds or as specified by dev

--- Unity Project/Assets/MakeItRainFish.cs
+++ Unity Project/Assets/MakeItRainFish.cs
@@ -1,42 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using NewtonVR;
 using UnityEngine;
 
 
-	public class MakeItRainFish : MonoBehaviour {
-		public GameObject[] FishPrefab;
+public class MakeItRainFish : MonoBehaviour
+{
+    public GameObject[] FishPrefab;
+    public float timeBetweenSpawn;
+
+    private float spawnTimer;
 
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start() {}
 
-	//	Rigidbody rb = RocketInstance.GetComponent<Rigidbody>();
-	//	rb.velocity = new Vector3(0,-10,0);
-        Debug.Log("Make it Rain Script");
+    // Update is called once per frame
+    void Update()
+    {
+        //	Rigidbody rb = RocketInstance.GetComponent<Rigidbody>();
+        //	rb.velocity = new Vector3(0,-10,0);
+        // Debug.Log("Make it Rain Script");
 
-		int randomGen = Random.Range (0, FishPrefab.Length);
-	    GameObject fish = GameObject.Instantiate(FishPrefab[randomGen], transform.position, transform.rotation);
+        if (spawnTimer > 0)
+        {
+            spawnTimer -= Time.deltaTime;
+            return;
+        }
+
+        spawnTimer = timeBetweenSpawn;
+
+        int randomGen = Random.Range(0, FishPrefab.Length);
+        GameObject fish = GameObject.Instantiate(FishPrefab[randomGen], transform.position, transform.rotation);
+       
+
+        fish.GetComponent<Rigidbody>().isKinematic = true;
+        fish.GetComponent<NVRInteractableItem>().CanAttach = false;
+
+        Vector3 startScale = Vector3.one * 0.1f;
+        Vector3 endScale = Vector3.one;
+
+        float startTime = Time.time;
+        float overTime = 0.5f;
+        float stopTime = startTime + overTime;
 
 
-		fish.GetComponent<Rigidbody>().isKinematic = true;
-		fish.GetComponent<NVRInteractableItem>().CanAttach = false;
-
-		Vector3 startScale = Vector3.one * 0.1f;
-		Vector3 endScale = Vector3.one;
-
-		float startTime = Time.time;
-		float overTime = 0.5f;
-		float stopTime = startTime + overTime;
-
-
-
-		fish.GetComponent<Rigidbody>().isKinematic = false;
-		fish.GetComponent<NVRInteractableItem>().CanAttach = true;
-	}
+        fish.GetComponent<Rigidbody>().isKinematic = false;
+        fish.GetComponent<NVRInteractableItem>().CanAttach = true;
+    }
 }
-
