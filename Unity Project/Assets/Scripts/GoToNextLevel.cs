5f333596b50a2540c258b57ad918fc8b19dfbe89
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-09-05 23:55:11 -0700

    Building Levels

---
+++ Unity Project/Assets/Scripts/GoToNextLevel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToNextLevel : MonoBehaviour
+{
+    [SerializeField] private string targetScene;
+    private bool isTouched;
+    
+
+    // Use this for initialization
+    void Start ()
+	{
+
+	    isTouched = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (isTouched)
+        {
+            SceneManager.LoadScene(targetScene);
+
+        }
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isTouched = true;
+    }
+}
