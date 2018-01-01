43a4f7859bc83dc8e9db8ebe70427e952b6cd60b
Jami Schwarzwalder <jschwarzwalder@users.noreply.github.com>
2018-01-01 04:14:31 -0800

    Save and Resume Buttons disappear when pressed

---
+++ Unity Project/Assets/GameLaunchButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using NewtonVR;
+
+public class GameLaunchButton : MonoBehaviour {
+
+    public NVRCanvasInput Player;
+    public Button StartButton;
+    public Button ContinueButton;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void isPushed()
+    {
+        // //turn off laser pointer
+        // Player.LaserEnabled = false;
+
+        //hide buttons
+        StartButton.gameObject.SetActive(false);
+        ContinueButton.gameObject.SetActive(false);
+
+
+    }
+}
