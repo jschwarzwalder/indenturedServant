8627cba0c01d0941a24fb709c58e2dae28b91b0c
Jami Schwarzwalder <jschwarzwalder@users.noreply.github.com>
2018-05-05 00:04:19 -0700

    Polishing the Vive version before Ifest Demo
    added Cheat to reset game or go to specific levels
    Removed Canvas interactable from NVR Player
    Can still teleport around level
    At the end if you touch to door you return to the home screen
    Fixed way you pick up fish in level 2

---
+++ Unity Project/Assets/SelectLevel.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        //Listen for key press
+
+        //if r, go to game home
+        if (Input.GetKeyDown("r")) {
+            SceneManager.LoadScene(0);
+        }
+        
+        //if 1-9 go to fish sorting level 1 to 9 or 0 will go to level 10
+        else if (Input.GetKeyDown("1")) {
+            SceneManager.LoadScene("Level 1");
+        }
+
+        else if (Input.GetKeyDown("2"))
+        {
+            SceneManager.LoadScene("Level 2");
+        }
+
+        else if (Input.GetKeyDown("3"))
+        {
+            SceneManager.LoadScene("Level 3");
+        }
+
+        else if (Input.GetKeyDown("4"))
+        {
+            SceneManager.LoadScene("Level 4");
+        }
+
+        else if (Input.GetKeyDown("5"))
+        {
+            SceneManager.LoadScene("Level 5");
+        }
+
+        else if (Input.GetKeyDown("6"))
+        {
+            SceneManager.LoadScene("Level 6");
+        }
+
+        else if (Input.GetKeyDown("7"))
+        {
+            SceneManager.LoadScene("Level 7");
+        }
+
+        else if (Input.GetKeyDown("8"))
+        {
+            SceneManager.LoadScene("Level 8");
+        }
+
+        else if (Input.GetKeyDown("9"))
+        {
+            SceneManager.LoadScene("Level 9");
+        }
+
+        else if (Input.GetKeyDown("0"))
+        {
+            SceneManager.LoadScene("Level 10");
+        }
+
+    }
+
+}
+
